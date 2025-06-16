7dc2766cacb9d2e2addc34232c3b6b478d602234
Gabriel Contreras <gabriel.contrerasv3@gmail.com>
2025-06-16 18:25:56 -0500

    improve roles execution and skills

--- sections/profile/Components/Profile/index.jsx
+++ sections/profile/Components/Profile/index.jsx
@@ -1,57 +1,8 @@
 import Image from "next/image";
 import { RoleChanger } from "../RoleChanger";
 import { CalendarLink } from "../CalendarButton";
+import profileData from "../../../data/profile";
 
-const profileData = [
-  {
-    lang: "es",
-    title: "Ingeniero de Sistemas",
-    description: `Ingeniero de Sistemas, SRE, Desarrollador, autodidacta apasionado por mejorar la vida de las personas mediante la tecnología, electrónica y los retos tecnológicos...`,
-    hardSkills: [
-      "Scrum", "Docker", "Bash", "Typescript", "Azure DevOps", "ReactJS", "SQL", "NodeJS", "PHP Laravel"
-    ],
-    softSkills: [
-      "Solución de Problemas", "Escucha Activa", "Persistencia", "Flexibilidad",
-      "Comunicación Asertiva", "Gestión del tiempo", "Gestión de conflictos", "Inteligencia emocional", "Trabajo en equipo"
-    ],
-    socialMedia: [
-      { href: "https://api.whatsapp.com/send?phone=3126004185", src: "/socialmedia/whatsapp.png", alt: "WhatsApp" },
-      { href: "https://github.com/gacodev", src: "/socialmedia/github.png", alt: "GitHub" },
-      { href: "https://www.linkedin.com/in/gabrielcontrerasv3", src: "/socialmedia/linkedin.webp", alt: "LinkedIn" },
-      { href: "mailto:gabriel.contrerasv3@gmail.com", src: "/socialmedia/gmail.png", alt: "Email" }
-    ],
-    cvTitle: "Curriculum Vitae",
-    cvButton: "Descargar",
-    contactPhone: "Teléfono:",
-    contactEmail: "Correo: ",
-    availableMessage: "¡Disponible para nuevos proyectos!",
-    calendarMessage: "¿Estás listo para agendar una reunión?",
-  },
-  {
-    lang: "en",
-    title: "Systems Engineer",
-    description: `Systems Engineer,SRE Developer, self-taught passionate about improving people's lives through technology, electronics, and technological challenges...`,
-    hardSkills: [
-      "Scrum", "Docker", "Bash", "TypeScript", "Azure DevOps", "ReactJS", "SQL", "NodeJS", "PHP Laravel", "Python"
-    ],
-    softSkills: [
-      "Problem Solving", "Active Listening", "Persistence", "Flexibility",
-      "Assertive Communication", "Time Management", "Conflict Management", "Emotional Intelligence", "Teamwork", "Leadership"
-    ],
-    socialMedia: [
-      { href: "https://api.whatsapp.com/send?phone=3126004185", src: "/socialmedia/whatsapp.png", alt: "WhatsApp", width: 40, height: 40 },
-      { href: "https://github.com/gacodev", src: "/socialmedia/github.png", alt: "GitHub", width: 40, height: 40 },
-      { href: "https://www.linkedin.com/in/gabrielcontrerasv3", src: "/socialmedia/linkedin.webp", alt: "LinkedIn", width: 40, height: 40 },
-      { href: "mailto:gabriel.contrerasv3@gmail.com", src: "/socialmedia/gmail.webp", alt: "Email", width: 40, height: 40 }
-    ],
-    cvTitle: "Curriculum Vitae",
-    cvButton: "Download",
-    contactPhone: "Phone:",
-    contactEmail: "Email: ",
-    availableMessage: "Available for new projects!",
-    calendarMessage: "Are you ready to schedule a meeting?",
-  },
-];
 
 export const ProfileContainer = ({ lang }) => {
   const profile = profileData.find((data) => data.lang === lang);
@@ -76,7 +27,7 @@ export const ProfileContainer = ({ lang }) => {
       <div className="flex flex-col md:flex-row justify-between p-1" id="profile">
         {/* Left Block */}
         <div className="md:w-1/2 p-4 bg-gray-800 bg-opacity-80 text-white rounded-lg shadow-md border-2 border-white flex flex-col items-center">
-          <h2 className="text-lg font-semibold text-center">{profile.title}</h2>
+          <h1 className="text-lg font-semibold text-center">{profile.title}</h1>
           <Image
             src="/gabriel.webp"
             priority
@@ -178,7 +129,10 @@ export const ProfileContainer = ({ lang }) => {
 
       {/* Profile Description */}
       <div className="mt-6 p-4 bg-gray-800 bg-opacity-80 text-white rounded-lg shadow-md flex justify-center">
-        <p className="text-center">{profile.description}</p>
+        <p className="text-left leading-relaxed tracking-wide text-lg">
+          {profile.description}
+        </p>
+
       </div>
     </div>
   );
